59b3b138c42a09623f310e9108d05ec5d7fb02e8
Steph Stone <hellostephstone@gmail.com>
2025-09-20 13:37:05 -0400

    PW test automation for API

--- tests/workingWithApi.spec.ts
+++ tests/workingWithApi.spec.ts
@@ -1,9 +1,80 @@
-import { test, expect } from "@playwright/test"
+import { test, expect } from '@playwright/test'
+import tags from '../test-data/tags.json'
+import { request } from 'http'
 
 test.beforeEach(async ({ page }) => {
-	await page.goto("https://conduit.bondaracademy.com/")
+	await page.route('*/**/api/tags', async (route) => {
+		await route.fulfill({
+			body: JSON.stringify(tags),
+		})
+	})
+
+	await page.goto('https://conduit.bondaracademy.com/')
+	await page.getByText('Sign in').click()
+	await page.getByRole('textbox', { name: 'Email' }).fill('pwtesttest1111@test.com')
+	await page.getByRole('textbox',{name: 'Password'}).fill('Welcome123')
+	await page.getByRole('button').click()
+
 })
 
-test("has title", async ({ page }) => {
-	await expect(page.locator(".navbar-brand")).toHaveText("conduit")
+test('has title', async ({ page }) => {
+	await page.route('*/**/api/articles*', async (route) => {
+		const response = await route.fetch()
+		const responseBody = await response.json()
+		responseBody.articles[0].title = "This is a MOCK test title"
+		responseBody.articles[0].description = "This is a MOCK description"
+
+		await route.fulfill({
+			body: JSON.stringify(responseBody),
+		})
+	})
+
+	await page.getByText('Global Feed').click()
+	await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+	await expect(page.locator('app-article-list h1').first()).toContainText(
+		'This is a MOCK test title'
+	)
+	await expect(page.locator('app-article-list p').first()).toContainText(
+		'This is a MOCK description'
+	)
+	// await page.waitForTimeout(1000) // use temporarily until you have assertions
+})
+
+test('delete article', async ({ page, request }) => {
+	const response = await request.post(
+		'https://conduit-api.bondaracademy.com/api/users/login',
+		{
+			data: {
+				"user": { "email": "pwtesttest1111@test.com", "password": "Welcome123" }
+			}
+		}
+	)
+	// Step 1: Make API call for Token
+	const responseBody = await response.json()
+	const accessToken = responseBody.user.token
+
+  const articleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles/', {
+		data: {
+			article: {
+				"title": "This is a test title",
+				"description": "This is a test description",
+				"body": "This is a test body",
+				"tagList": [],
+			},
+		},
+		headers: {
+			Authorization: `Token ${accessToken}`,
+		},
+	}) 
+
+	expect(articleResponse.status()).toEqual(201)
+
+	await page.getByText('Global Feed').click()
+	await page.getByText('This is a test title').click()
+	await page.getByRole('button', { name: "Delete Article"}).first().click()
+	await page.getByText('Global Feed').click()
+
+	await expect(page.locator('app-article-list p').first()).not.toContainText(
+		'This is a test title'
+	)
 })
